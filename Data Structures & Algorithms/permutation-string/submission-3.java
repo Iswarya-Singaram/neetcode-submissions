43f6ebca234b8839e8aba38caf3f8377a1deed5c
Iswarya-Singaram <145309713+Iswarya-Singaram@users.noreply.github.com>
2026-08-07 18:24:57 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        int[] c1 = new int[26];
+        int[] c2 = new int[26];
+        for(int i=0;i<s1.length();i++){
+            c1[s1.charAt(i)-'a']++;
+            c2[s2.charAt(i)-'a']++;
+        }
+        int matches=0;
+        for(int i=0;i<26;i++){
+            if(c1[i]==c2[i])
+            matches++;
+        }
+        int l=0;
+        for(int r = s1.length();r<s2.length();r++){
+            if(matches==26) return true;
+            int i = s2.charAt(r)-'a';
+            c2[i]++;
+            if(c2[i]==c1[i])
+            matches++;
+            else if(c1[i]+1 == c2[i])
+            matches--;
+            i = s2.charAt(l) - 'a';
+            c2[i]--;
+            if(c2[i]==c1[i]) matches++;
+            else if(c1[i]-1 == c2[i]) matches--;
+            l++;
+        }
+        return matches==26;
+
+    }
+}
